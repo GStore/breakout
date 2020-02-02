9d10ebc60953b17509e16c076211055f05a658ba
Gun Store <ggunstore@gmail.com>
2020-02-02 20:48:30 +0000

    Lesson 3: Collision detection with problems

--- src/lesson.ts
+++ src/lesson.ts
@@ -5,16 +5,31 @@ export class Breakout {
   private y!: number;
   private dx: number = 2;
   private dy: number = -2;
+  private ballRadius: number = 10;
 
   constructor(private ctx: ContextPath2D, private canvas: HTMLCanvasElement) {
     this.x = canvas.width/2;
     this.y = canvas.height/2;
   }
 
+  checkY = () => {
+    if(this.y + this.dy > this.canvas.height || this.y + this.dy < 0) {
+      this.dy = -this.dy;
+    }
+  }
+
+  checkX = () => {
+    if(this.x + this.dx > this.canvas.width || this.x + this.dx < 0) {
+      this.dx = -this.dx;
+    }
+  }
+
   draw = () => {
+    this.checkY();
+    this.checkX();
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle("#0095DD");
-    this.ctx.circle(this.x, this.y, 10, 0, Math.PI*2);
+    this.ctx.circle(this.x, this.y, this.ballRadius, 0, Math.PI*2);
     this.x += this.dx;
     this.y += this.dy;
   }
